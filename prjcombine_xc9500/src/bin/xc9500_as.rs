4ddd468444d735c491899d96d45c6350a76edc3c
Wanda <wanda@phinode.net>
2023-12-15 14:28:04 +0000

    add shit xc9500 assembler / disassembler

---
+++ prjcombine_xc9500/src/bin/xc9500_as.rs
@@ -0,0 +1,319 @@
+use std::{
+    error::Error,
+    fs::{read_to_string, File},
+    io::Write,
+    path::{Path, PathBuf},
+};
+
+use bitvec::vec::BitVec;
+use clap::Parser;
+use prjcombine_xc9500::{Database, Device, DeviceKind, FbBitCoord, GlobalBitCoord, Tile, TileItem};
+
+struct Bitstream {
+    fbs: Vec<Vec<[u8; 15]>>,
+    uim: Vec<Vec<Vec<[u8; 5]>>>,
+}
+
+impl Bitstream {
+    fn new(dev: &Device) -> Self {
+        let rows = if dev.kind == DeviceKind::Xc9500 {
+            72
+        } else {
+            108
+        };
+        let fbs = (0..dev.fbs)
+            .map(|_| {
+                vec![
+                    if dev.kind == DeviceKind::Xc9500 {
+                        [
+                            0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0xc0, 0, 0, 0, 0, 0, 0,
+                        ]
+                    } else {
+                        [0; 15]
+                    };
+                    rows
+                ]
+            })
+            .collect();
+        let uim = if dev.kind == DeviceKind::Xc9500 {
+            (0..dev.fbs)
+                .map(|_| (0..dev.fbs).map(|_| vec![[0; 5]; 18]).collect())
+                .collect()
+        } else {
+            vec![]
+        };
+        Bitstream { fbs, uim }
+    }
+
+    fn to_jed(&self) -> BitVec {
+        let mut res = BitVec::new();
+        if !self.uim.is_empty() {
+            for fb in 0..self.fbs.len() {
+                for row in 0..72 {
+                    for col in 0..15 {
+                        let sz = if col < 9 { 8 } else { 6 };
+                        for j in 0..sz {
+                            res.push((self.fbs[fb][row][col] >> j & 1) != 0);
+                        }
+                    }
+                }
+                for sfb in 0..self.fbs.len() {
+                    for row in 0..18 {
+                        for col in 0..5 {
+                            let sz = if col == 0 { 8 } else { 7 };
+                            for j in 0..sz {
+                                res.push((self.uim[fb][sfb][row][col] >> j & 1) != 0);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            for row in 0..108 {
+                for col in 0..15 {
+                    for fb in 0..self.fbs.len() {
+                        let sz = if col < 9 { 8 } else { 6 };
+                        for j in 0..sz {
+                            res.push((self.fbs[fb][row][col] >> j & 1) != 0);
+                        }
+                    }
+                }
+            }
+        }
+        res
+    }
+
+    fn put_bit(&mut self, fb: usize, row: usize, col: usize, bit: usize, val: bool) {
+        if val {
+            self.fbs[fb][row][col] |= 1 << bit;
+        } else {
+            self.fbs[fb][row][col] &= !(1 << bit);
+        }
+    }
+
+    fn put_global(&mut self, crd: GlobalBitCoord, val: bool) {
+        self.put_bit(
+            crd.fb as usize,
+            crd.row as usize,
+            crd.column as usize,
+            crd.bit as usize,
+            val,
+        );
+    }
+
+    fn put_fb(&mut self, fb: usize, crd: FbBitCoord, val: bool) {
+        self.put_bit(
+            fb,
+            crd.row as usize,
+            crd.column as usize,
+            crd.bit as usize,
+            val,
+        );
+    }
+
+    fn put_mc(&mut self, fb: usize, mc: usize, row: usize, val: bool) {
+        self.put_bit(fb, row, mc % 9, 6 + mc / 9, val);
+    }
+
+    fn put_pt(&mut self, fb: usize, mc: usize, pt: usize, imux: usize, pol: bool, val: bool) {
+        self.put_bit(
+            fb,
+            imux * 2 + usize::from(pol),
+            pt + (mc % 3) * 5,
+            mc / 3,
+            val,
+        );
+    }
+
+    fn put_uim(&mut self, fb: usize, sfb: usize, imux: usize, mc: usize, val: bool) {
+        if val {
+            self.uim[fb][sfb][mc][imux % 5] |= 1 << (imux / 5);
+        } else {
+            self.uim[fb][sfb][mc][imux % 5] &= !(1 << (imux / 5));
+        }
+    }
+}
+
+fn write_jed(fname: impl AsRef<Path>, dev: &str, bits: &BitVec) -> Result<(), Box<dyn Error>> {
+    let mut f = File::create(fname)?;
+    writeln!(f, "\x02QF{n}*", n = bits.len())?;
+    writeln!(f, "F0*")?;
+    writeln!(f, "N DEVICE {dev}*")?;
+    for (i, c) in bits.chunks(80).enumerate() {
+        write!(f, "L{ii:06} ", ii = i * 80)?;
+        for bit in c {
+            write!(f, "{x}", x = u32::from(*bit))?;
+        }
+        writeln!(f, "*")?;
+    }
+    writeln!(f, "\x030000")?;
+    Ok(())
+}
+
+fn set_tile_item<T: Copy>(
+    tile: &Tile<T>,
+    device: &Device,
+    item: &str,
+    mut put_bit: impl FnMut(T, bool),
+) {
+    let is_large = device.io_special.contains_key("GOE2");
+    if let Some((name, val)) = item.split_once('=') {
+        let item = tile.items.get(name).unwrap_or_else(|| {
+            &tile.items[&format!("{}.{}", name, if is_large { "LARGE" } else { "SMALL" })]
+        });
+        match item {
+            TileItem::Enum(item) => {
+                let val = &item.values[val];
+                for (k, v) in item.bits.iter().zip(val.iter()) {
+                    put_bit(*k, *v);
+                }
+            }
+            TileItem::BitVec(item) => {
+                assert_eq!(val.len(), item.bits.len());
+                for (k, v) in item.bits.iter().zip(val.chars().rev()) {
+                    put_bit(
+                        *k,
+                        match v {
+                            '0' => false,
+                            '1' => true,
+                            _ => unreachable!(),
+                        } ^ item.invert,
+                    )
+                }
+            }
+        }
+    } else {
+        let (name, val) = if let Some(name) = item.strip_prefix('!') {
+            (name, false)
+        } else {
+            (item, true)
+        };
+        let item = &tile.items[name];
+        match item {
+            TileItem::Enum(_) => unreachable!(),
+            TileItem::BitVec(item) => {
+                assert_eq!(item.bits.len(), 1);
+                put_bit(item.bits[0], val ^ item.invert);
+            }
+        }
+    }
+}
+
+#[derive(Parser)]
+struct Args {
+    dbdir: PathBuf,
+    src: PathBuf,
+    jed: PathBuf,
+}
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let src = read_to_string(args.src)?;
+    let mut lines = src.lines();
+    let mut dev = None;
+    for mut line in &mut lines {
+        if let Some(pos) = line.find('#') {
+            line = &line[..pos];
+        }
+        line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        let (pref, suf) = line.split_once(':').unwrap();
+        let suf = suf.trim();
+        assert_eq!(pref, "DEVICE");
+        dev = Some(suf);
+        break;
+    }
+    let dev = dev.unwrap();
+    let dbfn = if dev.ends_with("xv") {
+        args.dbdir.join("xc9500xv.zstd")
+    } else if dev.ends_with("xl") {
+        args.dbdir.join("xc9500xl.zstd")
+    } else {
+        args.dbdir.join("xc9500.zstd")
+    };
+    let db = Database::from_file(dbfn)?;
+    let mut part = None;
+    for p in &db.parts {
+        if p.name == dev {
+            part = Some(p);
+            break;
+        }
+    }
+    let Some(part) = part else {
+        eprintln!("Unknown device {dev}");
+        return Ok(());
+    };
+    let device = &db.devices[part.device];
+    let mut bs = Bitstream::new(device);
+    for mut line in lines {
+        if let Some(pos) = line.find('#') {
+            line = &line[..pos];
+        }
+        line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        let (pref, suf) = line.split_once(':').unwrap();
+        let pref: Vec<_> = pref.split_ascii_whitespace().collect();
+        let suf: Vec<_> = suf.trim().split_ascii_whitespace().collect();
+        let mut fb_bits = db.fb_bits.clone();
+        for (k, v) in device.imux_bits.clone().items {
+            fb_bits.items.insert(k, v);
+        }
+        match pref[..] {
+            ["GLOBAL"] => {
+                for item in suf {
+                    set_tile_item(&db.global_bits, device, item, |crd, val| {
+                        bs.put_global(crd, val)
+                    });
+                }
+            }
+            ["FB", fb] => {
+                let fb: usize = fb.parse()?;
+                for item in suf {
+                    set_tile_item(&fb_bits, device, item, |crd, val| bs.put_fb(fb, crd, val));
+                }
+            }
+            ["MC", fb, mc] => {
+                let fb: usize = fb.parse()?;
+                let mc: usize = mc.parse()?;
+                for item in suf {
+                    set_tile_item(&db.mc_bits, device, item, |crd, val| {
+                        bs.put_mc(fb, mc, crd as usize, val)
+                    });
+                }
+            }
+            ["PT", fb, mc, pt] => {
+                let fb: usize = fb.parse()?;
+                let mc: usize = mc.parse()?;
+                let pt: usize = pt.parse()?;
+                for item in suf {
+                    let (imux, pol) = if let Some(x) = item.strip_prefix('!') {
+                        (x.parse()?, false)
+                    } else {
+                        (item.parse()?, true)
+                    };
+                    bs.put_pt(fb, mc, pt, imux, pol, true);
+                }
+            }
+            ["UIM", fb, imux] => {
+                let fb: usize = fb.parse()?;
+                let imux: usize = imux.parse()?;
+                for item in suf {
+                    let (sfb, mc) = item.split_once('.').unwrap();
+                    let sfb: usize = sfb.parse()?;
+                    let mc: usize = mc.parse()?;
+                    bs.put_uim(fb, sfb, imux, mc, true);
+                }
+            }
+
+            _ => panic!("weird line {line}"),
+        }
+    }
+    let fuses = bs.to_jed();
+    write_jed(args.jed, dev, &fuses)?;
+
+    Ok(())
+}
---
+++ prjcombine_xc9500/src/bin/xc9500_dis.rs
@@ -0,0 +1,333 @@
+use std::{error::Error, path::PathBuf};
+
+use bitvec::vec::BitVec;
+use clap::Parser;
+use prjcombine_xc9500::{Database, Device, DeviceKind, FbBitCoord, GlobalBitCoord, Tile, TileItem};
+
+struct Bitstream {
+    fbs: Vec<Vec<[u8; 15]>>,
+    uim: Vec<Vec<Vec<[u8; 5]>>>,
+}
+
+impl Bitstream {
+    fn from_jed(fuses: &BitVec, device: &Device) -> Self {
+        let mut fbs = vec![];
+        let mut uim = vec![];
+        let mut pos = 0;
+        if device.kind == DeviceKind::Xc9500 {
+            for _ in 0..device.fbs {
+                let mut rows = vec![];
+                for _ in 0..72 {
+                    let mut row = [0; 15];
+                    for col in 0..15 {
+                        let sz = if col < 9 { 8 } else { 6 };
+                        let f = &fuses[pos..pos + sz];
+                        pos += sz;
+                        for j in 0..sz {
+                            if f[j] {
+                                row[col] |= 1 << j;
+                            }
+                        }
+                    }
+                    rows.push(row);
+                }
+                fbs.push(rows);
+                let mut uim_fb = vec![];
+                for _ in 0..device.fbs {
+                    let mut rows = vec![];
+                    for _ in 0..18 {
+                        let mut row = [0; 5];
+                        for col in 0..5 {
+                            let sz = if col == 0 { 8 } else { 7 };
+                            let f = &fuses[pos..pos + sz];
+                            pos += sz;
+                            for j in 0..sz {
+                                if f[j] {
+                                    row[col] |= 1 << j;
+                                }
+                            }
+                        }
+                        rows.push(row);
+                    }
+                    uim_fb.push(rows);
+                }
+                uim.push(uim_fb);
+            }
+        } else {
+            for _ in 0..device.fbs {
+                fbs.push(vec![[0; 15]; 108]);
+            }
+            for row in 0..108 {
+                for col in 0..15 {
+                    for fb in 0..device.fbs {
+                        let sz = if col < 9 { 8 } else { 6 };
+                        let f = &fuses[pos..pos + sz];
+                        pos += sz;
+                        for j in 0..sz {
+                            if f[j] {
+                                fbs[fb][row][col] |= 1 << j;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        assert_eq!(pos, fuses.len());
+        Bitstream { fbs, uim }
+    }
+
+    fn get_bit(&self, fb: usize, row: usize, col: usize, bit: usize) -> bool {
+        (self.fbs[fb][row][col] >> bit & 1) != 0
+    }
+
+    fn get_global(&self, crd: GlobalBitCoord) -> bool {
+        self.get_bit(
+            crd.fb as usize,
+            crd.row as usize,
+            crd.column as usize,
+            crd.bit as usize,
+        )
+    }
+
+    fn get_fb(&self, fb: usize, crd: FbBitCoord) -> bool {
+        self.get_bit(fb, crd.row as usize, crd.column as usize, crd.bit as usize)
+    }
+
+    fn get_mc(&self, fb: usize, mc: usize, row: usize) -> bool {
+        self.get_bit(fb, row, mc % 9, 6 + mc / 9)
+    }
+
+    fn get_pt(&self, fb: usize, mc: usize, pt: usize, imux: usize, pol: bool) -> bool {
+        self.get_bit(fb, imux * 2 + usize::from(pol), pt + (mc % 3) * 5, mc / 3)
+    }
+
+    fn get_uim(&self, fb: usize, sfb: usize, imux: usize, mc: usize) -> bool {
+        (self.uim[fb][sfb][mc][imux % 5] >> (imux / 5) & 1) != 0
+    }
+}
+
+#[derive(Parser)]
+struct Args {
+    dbdir: PathBuf,
+    jed: PathBuf,
+}
+
+fn parse_jed(jed: &str) -> (String, BitVec) {
+    let stx = jed.find('\x02').unwrap();
+    let etx = jed.find('\x03').unwrap();
+    let mut res = None;
+    let mut len = None;
+    let mut device = None;
+    for cmd in jed[stx + 1..etx].split('*') {
+        let cmd = cmd.trim();
+        if let Some(arg) = cmd.strip_prefix("QF") {
+            assert!(len.is_none());
+            let n: usize = arg.parse().unwrap();
+            len = Some(n);
+        } else if let Some(arg) = cmd.strip_prefix("N DEVICE ") {
+            device = Some(arg.to_string())
+        } else if let Some(arg) = cmd.strip_prefix('F') {
+            assert!(res.is_none());
+            let x: u32 = arg.parse().unwrap();
+            let x = match x {
+                0 => false,
+                1 => true,
+                _ => unreachable!(),
+            };
+            res = Some(BitVec::repeat(x, len.unwrap()));
+        } else if let Some(arg) = cmd.strip_prefix('L') {
+            let sp = arg.find(' ').unwrap();
+            let mut pos: usize = arg[..sp].parse().unwrap();
+            let v = res.as_mut().unwrap();
+            for c in arg[sp..].chars() {
+                match c {
+                    '0' => {
+                        v.set(pos, false);
+                        pos += 1;
+                    }
+                    '1' => {
+                        v.set(pos, true);
+                        pos += 1;
+                    }
+                    ' ' => (),
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+    (device.unwrap(), res.unwrap())
+}
+
+fn print_tile<T: Copy>(tile: &Tile<T>, device: &Device, get_bit: impl Fn(T) -> bool) {
+    let is_large = device.io_special.contains_key("GOE2");
+    for (name, item) in &tile.items {
+        let mut name = &name[..];
+        if let Some(n) = name.strip_suffix(".SMALL") {
+            if is_large {
+                continue;
+            }
+            name = n;
+        }
+        if let Some(n) = name.strip_suffix(".LARGE") {
+            if !is_large {
+                continue;
+            }
+            name = n;
+        }
+        match item {
+            TileItem::Enum(item) => {
+                print!(" {name}=");
+                let bits: BitVec = item.bits.iter().map(|&crd| get_bit(crd)).collect();
+                let mut found = false;
+                for (vn, val) in &item.values {
+                    if val == &bits {
+                        print!("{vn}");
+                        found = true;
+                        break;
+                    }
+                }
+                if !found {
+                    for bit in bits.iter().rev() {
+                        print!("{}", u8::from(*bit));
+                    }
+                }
+            }
+            TileItem::BitVec(item) => {
+                let bits: BitVec = item
+                    .bits
+                    .iter()
+                    .map(|&crd| get_bit(crd) ^ item.invert)
+                    .collect();
+                if item.bits.len() == 1 {
+                    if bits[0] {
+                        print!(" {name}");
+                    } else {
+                        print!(" !{name}");
+                    }
+                } else {
+                    print!(" {name}=");
+                    for bit in bits.iter().rev() {
+                        print!("{}", u8::from(*bit));
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn print_globals(bs: &Bitstream, db: &Database, device: &Device) {
+    print!("GLOBAL:");
+    print_tile(&db.global_bits, device, |crd| bs.get_global(crd));
+    println!();
+}
+
+fn print_fb(bs: &Bitstream, db: &Database, device: &Device) {
+    for fb in 0..device.fbs {
+        print!("FB {fb}:");
+        print_tile(&db.fb_bits, device, |crd| bs.get_fb(fb, crd));
+        print_tile(&device.imux_bits, device, |crd| bs.get_fb(fb, crd));
+        println!();
+    }
+}
+
+fn print_uim(bs: &Bitstream, _db: &Database, device: &Device) {
+    for fb in 0..device.fbs {
+        for imux in 0..36 {
+            let found = (0..device.fbs).any(|sfb| (0..18).any(|mc| bs.get_uim(fb, sfb, imux, mc)));
+            if !found {
+                continue;
+            }
+            print!("UIM {fb} {imux}:");
+            for sfb in 0..device.fbs {
+                for mc in 0..18 {
+                    if bs.get_uim(fb, sfb, imux, mc) {
+                        print!(" {sfb}.{mc}");
+                    }
+                }
+            }
+            println!();
+        }
+    }
+}
+
+fn print_pt(bs: &Bitstream, _db: &Database, device: &Device) {
+    let num_imux = if device.kind == DeviceKind::Xc9500 {
+        36
+    } else {
+        54
+    };
+    for fb in 0..device.fbs {
+        for mc in 0..18 {
+            for pt in 0..5 {
+                let found = (0..num_imux)
+                    .any(|i| bs.get_pt(fb, mc, pt, i, true) || bs.get_pt(fb, mc, pt, i, false));
+                if !found {
+                    continue;
+                }
+                print!("PT {fb} {mc} {pt}:");
+                for i in 0..num_imux {
+                    if bs.get_pt(fb, mc, pt, i, true) {
+                        print!(" {i}");
+                    }
+                    if bs.get_pt(fb, mc, pt, i, false) {
+                        print!(" !{i}");
+                    }
+                }
+                println!();
+            }
+        }
+    }
+}
+
+fn print_mc(bs: &Bitstream, db: &Database, device: &Device) {
+    for fb in 0..device.fbs {
+        for mc in 0..18 {
+            print!("MC {fb} {mc}:");
+            print_tile(&db.mc_bits, device, |crd| bs.get_mc(fb, mc, crd as usize));
+            println!();
+        }
+    }
+}
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let jed = std::fs::read_to_string(args.jed)?;
+    let (device, fuses) = parse_jed(&jed);
+    let device = device.to_ascii_lowercase();
+    let dev = if let Some(pos) = device.find('-') {
+        &device[..pos]
+    } else {
+        &device[..]
+    };
+    let dbfn = if dev.ends_with("xv") {
+        args.dbdir.join("xc9500xv.zstd")
+    } else if dev.ends_with("xl") {
+        args.dbdir.join("xc9500xl.zstd")
+    } else {
+        args.dbdir.join("xc9500.zstd")
+    };
+    let db = Database::from_file(dbfn)?;
+    let mut part = None;
+    for p in &db.parts {
+        if p.name == dev {
+            part = Some(p);
+            break;
+        }
+    }
+    let Some(part) = part else {
+        eprintln!("Unknown device {dev}");
+        return Ok(());
+    };
+    let device = &db.devices[part.device];
+    let bs = Bitstream::from_jed(&fuses, device);
+    println!("DEVICE: {dev}");
+    print_globals(&bs, &db, device);
+    // TODO: print UIM IBUF
+    print_fb(&bs, &db, device);
+    if device.kind == DeviceKind::Xc9500 {
+        print_uim(&bs, &db, device);
+    }
+    print_pt(&bs, &db, device);
+    print_mc(&bs, &db, device);
+    Ok(())
+}
