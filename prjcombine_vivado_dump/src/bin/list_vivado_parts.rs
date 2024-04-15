4a13dff58ade02f70bc10790d4f88469eb96cb81
Wanda <wanda@phinode.net>
2024-04-15 03:07:47 +0000

    more clap

--- prjcombine_vivado_dump/src/bin/list_vivado_parts.rs
+++ prjcombine_vivado_dump/src/bin/list_vivado_parts.rs
@@ -1,11 +1,20 @@
 use prjcombine_toolchain::Toolchain;
 use prjcombine_vivado_dump::parts::get_parts;
-use std::env;
 use std::error::Error;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(
+    name = "dump_vivado_parts",
+    about = "Dump Vivado part geometry into rawdump files."
+)]
+struct Args {
+    toolchain: String,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let tc = Toolchain::from_file(&args[1])?;
+    let args = Args::parse();
+    let tc = Toolchain::from_file(&args.toolchain)?;
     let parts = get_parts(&tc)?;
     for part in parts {
         println!("{part:?}");
