8f07219b6f14a481da802562bcf2916ea47ca95d
Wanda <wanda@phinode.net>
2025-05-04 18:59:25 +0000

    xcpld_speed: parallelize.

--- re/xilinx/cpld/src/bin/xcpld_speed/main.rs
+++ re/xilinx/cpld/src/bin/xcpld_speed/main.rs
@@ -4,6 +4,7 @@ mod xbr;
 mod xc9500;
 mod xpla3;
 
+use std::sync::Mutex;
 use std::{collections::HashSet, error::Error, path::PathBuf};
 
 use clap::Parser;
@@ -26,38 +27,49 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let tc = Toolchain::from_file(&args.toolchain)?;
     let db = Database::from_file(args.db)?;
+    let parts = Mutex::new(vec![]);
     let mut done = HashSet::new();
-    let mut parts = vec![];
-    for part in &db.parts {
-        if let Some(ref filter) = args.device {
-            if *filter != part.dev_name
-                && *filter != format!("{d}{p}", d = part.dev_name, p = part.pkg_name)
-            {
+    rayon::scope(|scope| {
+        for part in &db.parts {
+            if let Some(ref filter) = args.device {
+                if *filter != part.dev_name
+                    && *filter != format!("{d}{p}", d = part.dev_name, p = part.pkg_name)
+                {
+                    continue;
+                }
+            }
+            let device = &db.devices[part.device];
+            let package = &db.packages[part.package];
+            if done.contains(&part.dev_name) {
                 continue;
             }
+            done.insert(part.dev_name.clone());
+            for spd in &part.speeds {
+                let tc = &tc;
+                let parts = &parts;
+                scope.spawn(move |_| {
+                    println!("DEV {d} {p} {spd}", d = part.dev_name, p = part.pkg_name);
+                    let speed = match device.device.kind {
+                        DeviceKind::Xc9500 | DeviceKind::Xc9500Xl | DeviceKind::Xc9500Xv => {
+                            xc9500::test_xc9500(tc, part, &device.device, package, spd)
+                        }
+                        DeviceKind::Xpla3 => {
+                            xpla3::test_xpla3(tc, part, &device.device, package, spd)
+                        }
+                        DeviceKind::Coolrunner2 => {
+                            xbr::test_xbr(tc, part, &device.device, package, spd)
+                        }
+                    };
+                    parts.lock().unwrap().push(SpeedDbPart {
+                        dev_name: part.dev_name.clone(),
+                        speed_name: spd.clone(),
+                        speed,
+                    })
+                });
+            }
         }
-        let device = &db.devices[part.device];
-        let package = &db.packages[part.package];
-        if done.contains(&part.dev_name) {
-            continue;
-        }
-        done.insert(part.dev_name.clone());
-        for spd in &part.speeds {
-            println!("DEV {d} {p} {spd}", d = part.dev_name, p = part.pkg_name);
-            let speed = match device.device.kind {
-                DeviceKind::Xc9500 | DeviceKind::Xc9500Xl | DeviceKind::Xc9500Xv => {
-                    xc9500::test_xc9500(&tc, part, &device.device, package, spd)
-                }
-                DeviceKind::Xpla3 => xpla3::test_xpla3(&tc, part, &device.device, package, spd),
-                DeviceKind::Coolrunner2 => xbr::test_xbr(&tc, part, &device.device, package, spd),
-            };
-            parts.push(SpeedDbPart {
-                dev_name: part.dev_name.clone(),
-                speed_name: spd.clone(),
-                speed,
-            })
-        }
-    }
+    });
+    let parts = parts.into_inner().unwrap();
     let sdb = SpeedDb { parts };
     sdb.to_file(args.sdb)?;
     Ok(())
