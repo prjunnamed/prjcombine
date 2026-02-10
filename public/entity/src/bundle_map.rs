c8ec4105398e8c10733239636ffb52219eb4c1a0
Maja Kądziołka <maya@compilercrim.es>
2026-02-10 16:35:33 +0100

    EntityBundleMap: use strongly-typed IDs internally

--- public/entity/src/bundle_map.rs
+++ public/entity/src/bundle_map.rs
@@ -1,5 +1,6 @@
-use crate::{EntityId, EntityRange, EntityVec};
-use indexmap::IndexMap;
+use crate::{EntityId, EntityRange, EntityVec, EntityMap};
+use crate::map::Entry;
+use crate::id::{EntityTag, EntityIdU32};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum EntityBundleIndex<I: EntityId> {
@@ -13,10 +14,15 @@ pub enum EntityBundleItemIndex {
     Array { index: usize, total: usize },
 }
 
+struct BundleTag;
+impl EntityTag for BundleTag {}
+type BundleId = EntityIdU32<BundleTag>;
+
+/// A map where each `(K, V)` pair is assigned a contiguous range of IDs.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct EntityBundleMap<I: EntityId, T> {
-    ids: EntityVec<I, usize>,
-    bundles: IndexMap<String, (EntityBundleIndex<I>, T)>,
+    ids: EntityVec<I, BundleId>,
+    bundles: EntityMap<BundleId, String, (EntityBundleIndex<I>, T)>,
 }
 
 impl<I: EntityId, T> EntityBundleMap<I, T> {
@@ -40,13 +46,13 @@ impl<I: EntityId, T> EntityBundleMap<I, T> {
     }
 
     pub fn get(&self, key: &str) -> Option<(EntityBundleIndex<I>, &T)> {
-        let &(idx, ref val) = self.bundles.get(key)?;
-        Some((idx, val))
+        let (_, (idx, val)) = self.bundles.get(key)?;
+        Some((*idx, val))
     }
 
     pub fn get_mut(&mut self, key: &str) -> Option<(EntityBundleIndex<I>, &mut T)> {
-        let &mut (idx, ref mut val) = self.bundles.get_mut(key)?;
-        Some((idx, val))
+        let (_, (idx, val)) = self.bundles.get_mut(key)?;
+        Some((*idx, val))
     }
 
     pub fn contains_key(&self, key: &str) -> bool {
@@ -54,7 +60,9 @@ impl<I: EntityId, T> EntityBundleMap<I, T> {
     }
 
     pub fn key(&self, id: I) -> (&str, EntityBundleItemIndex) {
-        let (key, &(bidx, _)) = self.bundles.get_index(self.ids[id]).unwrap();
+        let idx = self.ids[id];
+        let key = self.bundles.key(idx);
+        let (bidx, _) = self.bundles[idx];
         match bidx {
             EntityBundleIndex::Single(sid) => {
                 assert_eq!(id, sid);
@@ -72,8 +80,8 @@ impl<I: EntityId, T> EntityBundleMap<I, T> {
 
     pub fn insert(&mut self, name: String, value: T) -> Option<I> {
         match self.bundles.entry(name) {
-            indexmap::map::Entry::Occupied(_) => None,
-            indexmap::map::Entry::Vacant(e) => {
+            Entry::Occupied(_) => None,
+            Entry::Vacant(e) => {
                 let id = self.ids.push(e.index());
                 e.insert((EntityBundleIndex::Single(id), value));
                 Some(id)
@@ -83,8 +91,8 @@ impl<I: EntityId, T> EntityBundleMap<I, T> {
 
     pub fn insert_array(&mut self, name: String, num: usize, value: T) -> Option<EntityRange<I>> {
         match self.bundles.entry(name) {
-            indexmap::map::Entry::Occupied(_) => None,
-            indexmap::map::Entry::Vacant(e) => {
+            Entry::Occupied(_) => None,
+            Entry::Vacant(e) => {
                 let id = self.ids.next_id();
                 let range = EntityRange::new(id.to_idx(), id.to_idx() + num);
                 for _ in 0..num {
@@ -106,17 +114,17 @@ impl<I: EntityId, T> EntityBundleMap<I, T> {
     pub fn bundles(&self) -> impl Iterator<Item = (EntityBundleIndex<I>, &str, &T)> {
         self.bundles
             .iter()
-            .map(|(k, &(i, ref v))| (i, k.as_str(), v))
+            .map(|(_, k, (i, v))| (*i, k.as_str(), v))
     }
 
     pub fn bundles_mut(&mut self) -> impl Iterator<Item = (EntityBundleIndex<I>, &str, &mut T)> {
         self.bundles
             .iter_mut()
-            .map(|(k, &mut (i, ref mut v))| (i, k.as_str(), v))
+            .map(|(_, k, (i, v))| (*i, k.as_str(), v))
     }
 
     pub fn into_bundles(self) -> impl Iterator<Item = (EntityBundleIndex<I>, String, T)> {
-        self.bundles.into_iter().map(|(k, (i, v))| (i, k, v))
+        self.bundles.into_iter().map(|(_, k, (i, v))| (i, k, v))
     }
 }
 
@@ -131,14 +139,14 @@ impl<I: EntityId, T> core::ops::Index<I> for EntityBundleMap<I, T> {
 
     fn index(&self, index: I) -> &Self::Output {
         let idx = self.ids[index];
-        &self.bundles.get_index(idx).unwrap().1.1
+        &self.bundles[idx].1
     }
 }
 
 impl<I: EntityId, T> core::ops::IndexMut<I> for EntityBundleMap<I, T> {
     fn index_mut(&mut self, index: I) -> &mut Self::Output {
         let idx = self.ids[index];
-        &mut self.bundles.get_index_mut(idx).unwrap().1.1
+        &mut self.bundles[idx].1
     }
 }
 
